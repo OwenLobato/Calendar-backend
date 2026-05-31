e8961302275325061476a67585d53c745f00dc1f
OwenLobato <owenlobatov@gmail.com>
2026-05-31 11:45:03 -0600

    feat(api): add cors support
    - Install cors package and add it to dependencies
    - Import and configure cors middleware in the main application file
    - Enable Cross-Origin Resource Sharing to allow requests from different domains

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const { dbConnection } = require('./database/config');
 require('dotenv').config();
 
@@ -8,6 +9,9 @@ const app = express();
 
 // DATABASE
 dbConnection();
+
+// CORS
+app.use(cors());
 
 // PUBLIC
 app.use(express.static('public'));
